06f0250dbe1a7125da5f54ea030bf34de1d0322d
laariane <48860436+laariane@users.noreply.github.com>
2024-12-22 22:34:10 +0000

    adding detail view

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index,name='index'),
     path('books/',views.BookListView.as_view(),name="books"),
     # path('authors'),
-    # path('book/<id>')
+    path('books/<pk>',views.BookDetailView.as_view(),name='book-detail')
     # path('author/<id>')
 ]
